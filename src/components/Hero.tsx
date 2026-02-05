2ff218e655afdea7f6e5e70c1377037f3bf777a0
mishthimahajan <mishthimahajan0@gmail.com>
2026-02-06 01:11:28 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -93,12 +93,13 @@ const Hero = () => {
 
   const downloadCV = () => {
   const link = document.createElement("a");
-  link.href = encodeURI("/Mishthi Mahajan - Resume(6).pdf");
+  link.href = "/mishthi_resume.pdf";
   link.download = "Mishthi_Mahajan_Resume.pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 };
+
 
 
   return (
